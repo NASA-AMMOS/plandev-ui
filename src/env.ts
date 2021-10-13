e595c83ce32ece0e1790f7db5ef5c4a961509fbf
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2021-10-13 14:18:48 -0700

    refactor: Hasura migration
    - Migrate all queries to Hasura
    - Major upgrade

--- src/env.ts
+++ src/env.ts
@@ -2,8 +2,9 @@ export const CAM_API_URL: string = import.meta***REMOVED***.VITE_CAM_API_URL as 
 
 export const CAM_ENABLED: boolean = import.meta***REMOVED***.VITE_CAM_ENABLED as boolean;
 
-export const GATEWAY_APOLLO_URL: string = import.meta***REMOVED***
-  .VITE_GATEWAY_APOLLO_URL as string;
+export const GATEWAY_URL: string = import.meta***REMOVED***.VITE_GATEWAY_URL as string;
+
+export const HASURA_URL: string = import.meta***REMOVED***.VITE_HASURA_URL as string;
 
 export const POSTGRES_DATABASE: string = import.meta***REMOVED***
   .VITE_POSTGRES_DATABASE as string;
