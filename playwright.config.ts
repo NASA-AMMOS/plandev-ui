b6d850bc0833fd6c51b661a9b10842355d93e136
Chris <camargo.cac@gmail.com>
2022-04-28 19:06:00 -0700

    test: plan page (#22)
    - Add first cut of plan page test and fixture
    - Set Playwright to be fully parallel
    - Set Playwright to only retry and record in CI
    - Update banananation jar

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,19 +2,20 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process***REMOVED***.CI,
+  fullyParallel: true,
   reporter: [
     [process***REMOVED***.CI ? 'github' : 'list'],
     ['html', { open: 'never', outputFile: 'index.html', outputFolder: 'test-results' }],
     ['json', { outputFile: 'test-results/json-results.json' }],
     ['junit', { outputFile: 'test-results/junit-results.xml' }],
   ],
-  retries: 1,
+  retries: process***REMOVED***.CI ? 1 : 0,
   testDir: './tests',
   use: {
     baseURL: 'http://localhost:3000',
     browserName: 'chromium',
-    trace: 'on-first-retry',
-    video: 'on-first-retry',
+    trace: process***REMOVED***.CI ? 'on-first-retry' : 'off',
+    video: process***REMOVED***.CI ? 'on-first-retry' : 'off',
   },
   webServer: {
     command: 'node build',
