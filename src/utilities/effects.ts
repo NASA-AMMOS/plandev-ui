3f94f7be032619f4e6eb05785e2791419c4ca355
JosephVolosin <joseph.p.volosin@nasa.gov>
2024-11-18 15:46:59 -0500

    Close creation modal after successful creation

--- src/utilities/effects.ts
+++ src/utilities/effects.ts
@@ -893,6 +893,7 @@ const effects = {
         createExternalEventTypeError.set((e as Error).message);
       }
       catchError(e as Error);
+      return null;
     }
   },
 
@@ -907,7 +908,6 @@ const effects = {
     validAt: string,
     user: User | null,
   ) {
-    // TODO: how much of this should be done here vs. in gateway?
     try {
       if (!gatewayPermissions.CREATE_EXTERNAL_SOURCE(user)) {
         throwPermissionError('upload an external source');
@@ -1062,6 +1062,7 @@ const effects = {
         createExternalSourceTypeError.set((e as Error).message);
       }
       catchError(e as Error);
+      return null;
     }
   },
 
