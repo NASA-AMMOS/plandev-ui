dbd10fe0ade37e7e02d47b84f3bcad3f8819da02
Aaron Plave <aaron.plave@jpl.nasa.gov>
2024-12-02 09:22:41 -0800

    Fixes and refactoring

--- src/components/timeline/form/TimelineEditor/ActivityFilterBuilder.svelte
+++ src/components/timeline/form/TimelineEditor/ActivityFilterBuilder.svelte
@@ -419,6 +419,7 @@
                             does_not_include: { type: 'tag', values: $subsystemTags },
                             includes: { type: 'tag', values: $subsystemTags },
                           },
+                          // TODO make this a searchable dropdown, same for parameter variants?
                           Type: {
                             does_not_equal: { type: 'variant', values: $activityTypes.map(type => type.name) },
                             does_not_include: { type: 'string' },
