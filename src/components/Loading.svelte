f16e80a0c6545aaf0fcf6bed4b896a66644ceef9
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-01-03 14:43:28 -0800

    Add loading text component

---
+++ src/components/Loading.svelte
@@ -0,0 +1,10 @@
+<div class="st-typography-label loading">Loading...</div>
+
+<style>
+  .loading {
+    display: flex;
+    flex-direction: column;
+    user-select: none;
+    width: 100%;
+  }
+</style>
