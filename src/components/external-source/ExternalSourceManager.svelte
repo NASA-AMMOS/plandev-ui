fe461d8d99f236ac0da40587dcd86505beb98ba0
JosephVolosin <joseph.p.volosin@nasa.gov>
2025-01-23 10:57:18 -0500

    Fix linting

--- src/components/external-source/ExternalSourceManager.svelte
+++ src/components/external-source/ExternalSourceManager.svelte
@@ -524,10 +524,8 @@
       if (newSourceExternalEventTypes[event.event_type] === undefined) {
         newSourceExternalEventTypes[event.event_type] = Object.keys(event.attributes);
       } else if (newSourceExternalEventTypes[event.event_type].length === Object.keys(event.attributes).length) {
-        const attributeInconsistencies = currentKeySet.difference(
-          new Set(newSourceExternalEventTypes[event.event_type]),
-        );
-        if (attributeInconsistencies.size !== 0) {
+        const attributeConsistency = currentKeySet.intersection(new Set(newSourceExternalEventTypes[event.event_type]));
+        if (attributeConsistency.size !== currentKeySet.size) {
           uploadDisabledMessage = 'Event attributes are inconsistent across events of type ' + event.event_type + '.';
           return false;
         }
