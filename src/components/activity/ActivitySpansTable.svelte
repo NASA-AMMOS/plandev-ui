155c23fbab712344c58c478a6050552fe29a69ec
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-01-27 17:04:50 -0800

    Refactor

--- src/components/activity/ActivitySpansTable.svelte
+++ src/components/activity/ActivitySpansTable.svelte
@@ -2,7 +2,6 @@
 
 <script lang="ts">
   import type { ColDef, ColumnState } from 'ag-grid-community';
-  import { initialSpansLoading } from '../../stores/simulation';
   import type { RowId } from '../../types/data-grid';
   import type { Span, SpanId } from '../../types/simulation';
   import DataGrid from '../ui/DataGrid/DataGrid.svelte';
@@ -10,6 +9,7 @@
   export let columnDefs: ColDef[];
   export let columnStates: ColumnState[] = [];
   export let dataGrid: DataGrid<Span> | undefined = undefined;
+  export let loading: boolean = false;
   export let selectedSpanId: SpanId | null = null;
   export let spans: Span[] | null | undefined = undefined;
   export let filterExpression: string = '';
@@ -38,7 +38,7 @@
   {getRowId}
   useCustomContextMenu
   rowData={spans || []}
-  loading={$initialSpansLoading}
+  {loading}
   rowSelection="single"
   scrollToSelection={true}
   suppressDragLeaveHidesColumns={false}
