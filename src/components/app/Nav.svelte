ddd0132f67e9e341279f8cbae397fa0e74948f0e
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-02-03 10:34:47 -0800

    WIP

--- src/components/app/Nav.svelte
+++ src/components/app/Nav.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import { Button, Command, Select } from '@nasa-jpl/stellar-svelte';
   import AppMenu from '../../components/menus/AppMenu.svelte';
+
   import type { User, UserRole } from '../../types/app';
   import { getTarget } from '../../utilities/generic';
   import { changeUserRole } from '../../utilities/permissions';
@@ -11,6 +13,7 @@
   $: userRoles = user?.allowedRoles ?? [];
 
   async function changeRole(event: Event) {
+    console.log('CHANGR ROLE', event);
     const { value } = getTarget(event);
     if (value) {
       await changeUserRole(value as string);
@@ -28,14 +31,53 @@
     </div>
     <slot name="left" />
   </div>
+  <script lang="ts">
+    import * as Command from '$lib/components/ui/command';
+  </script>
+  <Command.Root>
+    <Command.Input placeholder="Type a command or search..." />
+    <Command.List>
+      <Command.Empty>No results found.</Command.Empty>
+      <Command.Group heading="Suggestions">
+        <Command.Item>Calendar</Command.Item>
+        <Command.Item>Search Emoji</Command.Item>
+        <Command.Item>Calculator</Command.Item>
+      </Command.Group>
+      <Command.Separator />
+      <Command.Group heading="Settings">
+        <Command.Item>Profile</Command.Item>
+        <Command.Item>Billing</Command.Item>
+        <Command.Item>Settings</Command.Item>
+      </Command.Group>
+    </Command.List>
+  </Command.Root>
+
+  <Button variant="s"></Button>
+  <!-- <Avatar.Root>
+    <Avatar.Image srcssdf="sdfsdf" el />
+  </Avatar.Root> -->
   <div class="right">
     <slot name="right" />
     {#if userRoles.length > 1}
-      <select value={user?.activeRole} class="st-select" on:change={changeRole}>
+      <!-- <select value={user?.activeRole} class="st-select" on:change={changeRole}>
         {#each userRoles as userRole}
           <option value={userRole}>{userRole}</option>
         {/each}
-      </select>
+      </select> -->
+      <Select.Root value={user?.activeRole} onValueChange={changeRole}>
+        <Select.Trigger class="w-[180px]">
+          <Select.Value placeholder="Select a fruit" />
+        </Select.Trigger>
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Fruits</Select.Label>
+            {#each userRoles as userRole}
+              <Select.Item value={userRole} label={userRole}>{userRole}</Select.Item>
+            {/each}
+          </Select.Group>
+        </Select.Content>
+        <Select.Input name="favoriteFruit" />
+      </Select.Root>
     {/if}
   </div>
 </div>
