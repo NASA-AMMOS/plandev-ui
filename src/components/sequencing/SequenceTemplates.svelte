75c8f799208a91ff5db44fc812f7966173ca1594
David Legg <david.legg@jpl.nasa.gov>
2025-02-20 18:06:45 -0800

    WIP - building sequence template editor

---
+++ src/components/sequencing/SequenceTemplates.svelte
@@ -0,0 +1,154 @@
+<svelte:options immutable={true} />
+
+<script lang="ts">
+  import { browser } from '$app/environment';
+  import { goto } from '$app/navigation';
+  import { base } from '$app/paths';
+  import { onMount } from 'svelte';
+  import { SearchParameters } from '../../enums/searchParameters';
+  import { parcels, userSequences, userSequencesColumns, workspaces } from '../../stores/sequencing';
+  import type { User } from '../../types/app';
+  import type { Parcel, UserSequence, Workspace } from '../../types/sequencing';
+  import { satfToSequence } from '../../utilities/codemirror/satf/satf-sasf-utils';
+  import effects from '../../utilities/effects';
+  import { getSearchParameterNumber, setQueryParam } from '../../utilities/generic';
+  import { permissionHandler } from '../../utilities/permissionHandler';
+  import { featurePermissions } from '../../utilities/permissions';
+  import Input from '../form/Input.svelte';
+  import CssGrid from '../ui/CssGrid.svelte';
+  import CssGridGutter from '../ui/CssGridGutter.svelte';
+  import Panel from '../ui/Panel.svelte';
+  import SectionTitle from '../ui/SectionTitle.svelte';
+  import WorkspaceTable from '../workspace/WorkspaceTable.svelte';
+  import SequenceTable from './SequenceTable.svelte';
+  import SequenceTemplateEditor from './SequenceTemplateEditor.svelte';
+
+  export let user: User | null;
+
+  let filterText: string = '';
+  let parcel: Parcel | null;
+  let selectedSequence: UserSequence | null = null;
+  let workspace: Workspace | undefined;
+  let workspaceId: number | null = null;
+
+  $: parcel = $parcels.find(p => p.id === selectedSequence?.parcel_id) ?? null;
+  $: workspace = $workspaces.find(workspace => workspace.id === workspaceId);
+  $: if (selectedSequence !== null) {
+    const found: number = $userSequences.findIndex(sequence => sequence.id === selectedSequence?.id);
+
+    if (found === -1) {
+      selectedSequence = null;
+    }
+  }
+
+  onMount(() => {
+    workspaceId = getSearchParameterNumber(SearchParameters.WORKSPACE_ID);
+  });
+
+  function onSequenceSelected(event: CustomEvent<UserSequence>) {
+    selectedSequence = event.detail;
+  }
+
+  function onWorkspaceSelected(event: CustomEvent<number>) {
+    workspaceId = event.detail;
+
+    if (browser) {
+      setQueryParam(SearchParameters.WORKSPACE_ID, workspaceId !== null ? `${workspaceId}` : null);
+    }
+  }
+
+  function navigateToNewSequence(): void {
+    const workspaceId = getSearchParameterNumber(SearchParameters.WORKSPACE_ID);
+    goto(`${base}/sequencing/new${workspaceId ? `?${SearchParameters.WORKSPACE_ID}=${workspaceId}` : ''}`);
+  }
+
+  async function importLibrary(): Promise<void> {
+    const library = await effects.importLibrarySequences(workspaceId);
+    if (!library) {
+      return;
+    }
+
+    const parcel = library.parcel;
+    const sequences = (await satfToSequence(library.fileContents)).sequences;
+    sequences.forEach(async seqN => {
+      await effects.createUserSequence(
+        {
+          definition: seqN.sequence,
+          name: seqN.name,
+          parcel_id: parcel,
+          seq_json: '',
+          workspace_id: workspaceId !== null ? workspaceId : -1,
+        },
+        user,
+      );
+    });
+  }
+</script>
+
+<CssGrid bind:columns={$userSequencesColumns}>
+  <WorkspaceTable {user} selectedWorkspaceId={workspace?.id} on:workspaceSelected={onWorkspaceSelected} />
+
+  <CssGridGutter track={1} type="column" />
+
+  <Panel>
+    <svelte:fragment slot="header">
+      <SectionTitle>Sequence Templates</SectionTitle>
+
+      <Input>
+        <input bind:value={filterText} class="st-input" placeholder="Filter sequences" style="width: 100%;" />
+      </Input>
+
+      <div class="right">
+        <button
+          class="st-button secondary ellipsis"
+          use:permissionHandler={{
+            hasPermission: featurePermissions.sequences.canCreate(user),
+            permissionError: 'You do not have permission to create a new sequence',
+          }}
+          disabled={workspace === undefined}
+          on:click={navigateToNewSequence}
+        >
+          New Template
+        </button>
+
+        <button
+          class="st-button secondary ellipsis"
+          use:permissionHandler={{
+            hasPermission: featurePermissions.sequences.canCreate(user),
+            permissionError: 'You do not have permission to upload library sequences',
+          }}
+          disabled={workspace === undefined}
+          on:click|stopPropagation={importLibrary}
+        >
+          Import Library
+        </button>
+      </div>
+    </svelte:fragment>
+
+    <svelte:fragment slot="body">
+      <SequenceTable {filterText} {user} {workspace} on:sequenceSelected={onSequenceSelected} />
+    </svelte:fragment>
+  </Panel>
+
+  <CssGridGutter track={3} type="column" />
+
+  <SequenceTemplateEditor
+    {parcel}
+    showCommandFormBuilder={false}
+    sequenceDefinition={selectedSequence?.definition ?? ''}
+    sequenceName={selectedSequence?.name}
+    sequenceOutput={selectedSequence?.seq_json}
+    title="Sequence Template - Editor (Read-only)"
+    readOnly={true}
+    {workspaceId}
+    {user}
+  />
+</CssGrid>
+
+<style>
+  .right {
+    column-gap: 5px;
+    display: flex;
+    flex-wrap: nowrap;
+  }
+</style>
