80d610256ea810393c6dbb1fb506c1524bc9849b
David Legg <david.legg@jpl.nasa.gov>
2025-02-20 18:06:45 -0800

    WIP - building sequence template editor

---
+++ src/routes/sequencing/templates/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:options immutable={true} />
+
+<script lang="ts">
+  import type { PageData } from '../$types';
+  import PageTitle from '../../../components/app/PageTitle.svelte';
+  import SequenceTemplates from '../../../components/sequencing/SequenceTemplates.svelte';
+
+  export let data: PageData;
+</script>
+
+<PageTitle title="Sequencing" />
+
+<SequenceTemplates user={data.user} />
