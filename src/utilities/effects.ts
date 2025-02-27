7924e1564718cdd59ec17dfa6f1fbb879af3dcd2
psubram3 <pranav.subramanian@nasa.gov>
2025-02-27 09:06:46 -0800

    fix with rebase

--- src/utilities/effects.ts
+++ src/utilities/effects.ts
@@ -3951,7 +3951,6 @@ const effects = {
         user,
       );
 
-      console.log(data);
       const { expanded_sequences } = data;
       if (expanded_sequences != null && expanded_sequences.length === 1) {
         const { expanded_sequence } = expanded_sequences[0];
