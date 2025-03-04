8342e12c6a091f573ccbd222bf1c5f2492b3f213
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-03-04 15:05:34 -0800

    Update action example

--- e2e-tests/data/aerie-action-demo.js
+++ e2e-tests/data/aerie-action-demo.js
@@ -1,43 +1,57 @@
+'use strict';
+
 // API for reading/writing sequences (& other files?) in workspace
 async function listFiles() {
-  console.warn(`List files - Not yet implemented`);
-  return ["not implemented"];
+    console.warn(`List files - Not yet implemented`);
+    return ["not implemented"];
 }
 async function readFile(path) {
-  console.warn(`Read file ${path} - Not yet implemented`);
-  return "not implemented";
+    console.warn(`Read file ${path} - Not yet implemented`);
+    return "not implemented";
 }
 async function writeFile(path, contents) {
-  console.warn(`Write "${contents.slice(0, 50)}..." to ${path} - Not yet implemented`);
+    console.warn(`Write "${contents.slice(0, 50)}..." to ${path} - Not yet implemented`);
 }
 
 // import { ActionMain } from 'aerie-action/src/models/main.ts';
+// Define schemas for your action's settings and parameters
+const paramDefs = {
+    sequenceId: { type: "string" },
+    myBool: { type: "boolean" }
+};
+const settingDefs = {
+    externalUrl: { type: "string" },
+    retries: { type: "int" }
+};
 async function main(actionParameters, actionSettings) {
-  const url = `${actionSettings.externalUrl}/${actionParameters.sequenceId}`;
-  const startTime = performance.now();
-  // Make a request to an external URL using fetch
-  const result = await fetch(url, {
-      method: 'get',
-      headers: {
-          'Content-Type': 'application/json'
-      }
-  });
-  console.log(`request took ${performance.now() - startTime}ms`);
-  // try parsing result as either json or text
-  let resultData;
-  try {
-      resultData = await result.clone().json();
-  }
-  catch {
-      resultData = await result.clone().text();
-  }
-  // read/write files using the actions helpers
-  listFiles();
-  readFile('my_file');
-  writeFile('new_file', 'new contents');
-  return {
-      status: "SUCCESS",
-      data: resultData
-  };
+    const url = `${actionSettings.externalUrl}/${actionParameters.sequenceId}`;
+    const startTime = performance.now();
+    // Make a request to an external URL using fetch
+    const result = await fetch(url, {
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    console.log(`request took ${performance.now() - startTime}ms`);
+    // try parsing result as either json or text
+    let resultData;
+    try {
+        resultData = await result.clone().json();
+    }
+    catch {
+        resultData = await result.clone().text();
+    }
+    // read/write files using the actions helpers
+    listFiles();
+    readFile("my_file");
+    writeFile("new_file", "new contents");
+    return {
+        status: "SUCCESS",
+        data: resultData,
+    };
 }
 
+exports.main = main;
+exports.paramDefs = paramDefs;
+exports.settingDefs = settingDefs;
