27d838986f32822db685d907a0567dc2f9eb54b9
JosephVolosin <josephvolosin@gmail.com>
2025-03-04 09:30:21 -0500

    Selected sequence details

--- src/components/sequencing/SequenceTemplates.svelte
+++ src/components/sequencing/SequenceTemplates.svelte
@@ -8,6 +8,7 @@
   import { showTemplateModal } from '../../utilities/modal';
   import { permissionHandler } from '../../utilities/permissionHandler';
   import { featurePermissions } from '../../utilities/permissions';
+  import { tooltip } from '../../utilities/tooltip';
   import Input from '../form/Input.svelte';
   import CssGrid from '../ui/CssGrid.svelte';
   import CssGridGutter from '../ui/CssGridGutter.svelte';
@@ -15,12 +16,16 @@
   import SectionTitle from '../ui/SectionTitle.svelte';
   import SequenceTemplateEditor from './SequenceTemplateEditor.svelte';
   import SequenceTemplateTable from './SequenceTemplateTable.svelte';
+  import XIcon from 'bootstrap-icons/icons/x.svg?component';
 
   export let user: User | null;
 
   let filterText: string = '';
   let parcel: Parcel | null;
   let selectedTemplate: SequenceTemplate | null = null;
+  let sequenceTemplateRows: string;
+
+  $: sequenceTemplateRows = selectedTemplate !== null ? "1fr 3px 1fr" : "none";
 
   $: parcel = $parcels.find(p => p.id === selectedTemplate?.parcel_id) ?? null;
 
@@ -51,32 +56,85 @@
 </script>
 
 <CssGrid bind:columns={$userSequencesColumns}>
-  <Panel>
-    <svelte:fragment slot="header">
-      <SectionTitle>Sequence Templates</SectionTitle>
+  <CssGrid rows={sequenceTemplateRows}>
+    <Panel>
+      <svelte:fragment slot="header">
+        <SectionTitle>Sequence Templates</SectionTitle>
 
-      <Input>
-        <input bind:value={filterText} class="st-input" placeholder="Filter templates" style="width: 100%;" />
-      </Input>
+        <Input>
+          <input bind:value={filterText} class="st-input" placeholder="Filter templates" style="width: 100%;" />
+        </Input>
 
-      <div class="right">
-        <button
-          class="st-button secondary ellipsis"
-          use:permissionHandler={{
-            hasPermission: featurePermissions.sequences.canCreate(user),
-            permissionError: 'You do not have permission to create a new sequence',
-          }}
-          on:click|stopPropagation={createSequenceTemplate}
-        >
-          New Template
-        </button>
-      </div>
-    </svelte:fragment>
+        <div class="right">
+          <button
+            class="st-button secondary ellipsis"
+            use:permissionHandler={{
+              hasPermission: featurePermissions.sequences.canCreate(user),
+              permissionError: 'You do not have permission to create a new sequence',
+            }}
+            on:click|stopPropagation={createSequenceTemplate}
+          >
+            New Template
+          </button>
+        </div>
+      </svelte:fragment>
 
-    <svelte:fragment slot="body">
-      <SequenceTemplateTable {filterText} {user} on:templateSelected={onTemplateSelected} />
-    </svelte:fragment>
-  </Panel>
+      <svelte:fragment slot="body">
+        <SequenceTemplateTable {filterText} {user} on:templateSelected={onTemplateSelected} />
+      </svelte:fragment>
+    </Panel>
+
+
+    {#if selectedTemplate}
+      <CssGridGutter track={1} type="row" />
+      <Panel>
+        <svelte:fragment slot="header">
+          <slot name="left">
+            <SectionTitle>Sequence Template Details</SectionTitle>
+          </slot>
+          <slot name="right">
+            <button
+              class="st-button icon fs-6"
+              on:click={() => selectedTemplate = null}
+              use:tooltip={{ content: 'Deselect sequence template', placement: 'top' }}
+            >
+              <XIcon />
+          </button>
+          </slot>
+        </svelte:fragment>
+        <svelte:fragment slot="body">
+          <Input layout="inline">
+            Id
+            <input class="st-input w-100" disabled={true} name="id" value={selectedTemplate.id} />
+          </Input>
+          <Input layout="inline">
+            Name
+            <input class="st-input w-100" disabled={true} name="name" value={selectedTemplate.name} />
+          </Input>
+          <Input layout="inline">
+            Owner
+            <input class="st-input w-100" disabled={true} name="owner" value={selectedTemplate.owner} />
+          </Input>
+          <Input layout="inline">
+            Model Id
+            <input class="st-input w-100" disabled={true} name="modelId" value={selectedTemplate.model_id} />
+          </Input>
+          <Input layout="inline">
+            Parcel Id
+            <input class="st-input w-100" disabled={true} name="parcelId" value={selectedTemplate.parcel_id} />
+          </Input>
+          <Input layout="inline">
+            Language
+            <input class="st-input w-100" disabled={true} name="language" value={selectedTemplate.language} />
+          </Input>
+          <Input layout="inline">
+            Activity Type
+            <input class="st-input w-100" disabled={true} name="activityType" value={selectedTemplate.activity_type} />
+          </Input>
+        </svelte:fragment>
+      </Panel>
+    {/if}
+  </CssGrid>
 
   <CssGridGutter track={1} type="column" />
 
