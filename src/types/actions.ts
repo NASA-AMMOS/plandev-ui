81023dd061912b8b1c8f234bb57963a33d763693
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-03-12 14:27:55 -0700

    Type updates

--- src/types/actions.ts
+++ src/types/actions.ts
@@ -33,41 +33,4 @@ export type ActionRun = {
 
 export type ActionRunSlim = Omit<ActionRun, 'action_definition'>;
 
-// From https://github.com/NASA-AMMOS/aerie/blob/feature/action-server-dan-changes/action-server/src/type/types.ts
-export type ActionRunRequest = {
-  // actionJS: string;
-  parameters: any;
-  settings: any;
-};
-
-/* TODO: ActionResults should be defined by the actions API and imported */
-export type ActionResults = {
-  data: any;
-  status: 'FAILED' | 'SUCCESS';
-};
-
-export type ConsoleOutput = {
-  debug: string[];
-  error: string[];
-  info: string[];
-  log: string[];
-  warn: string[];
-};
-
-export type ActionResponse =
-  | {
-      console: ConsoleOutput;
-      errors: null;
-      results: ActionResults;
-    }
-  | {
-      console: ConsoleOutput;
-      errors: {
-        cause: unknown;
-        message: string;
-        stack: string | undefined;
-      }; // TODO: should this be an error array
-      results: null;
-    };
-
 export type ActionDefinitionSetInput = Pick<ActionDefinition, 'name' | 'description'>;
