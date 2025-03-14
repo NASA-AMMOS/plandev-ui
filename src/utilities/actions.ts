96e5f2bfca139d9e0987a582fbfda3336810172a
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-03-14 15:09:30 -0700

    Docs

--- src/utilities/actions.ts
+++ src/utilities/actions.ts
@@ -1,6 +1,9 @@
 import type { ActionDefinition } from '../types/actions';
 import type { ParametersMap } from '../types/parameter';
 
+/**
+ * Transforms a value schema record to a parameters map
+ */
 export function valueSchemaRecordToParametersMap(
   valueSchemaRecord: ActionDefinition['parameter_schema'],
 ): ParametersMap {
