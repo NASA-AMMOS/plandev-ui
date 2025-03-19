fa9965c5bfc06077b9c8807cd161f8148a1a2a3e
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-03-19 14:56:34 -0700

    Remove test pause

--- e2e-tests/fixtures/Action.ts
+++ e2e-tests/fixtures/Action.ts
@@ -29,7 +29,6 @@ export class Action {
     await expect(this.page.getByRole('button', { name: 'Create' })).toBeDisabled();
     await this.actionFormName.fill(this.actionName);
     await this.actionFormDescription.fill(this.actionDescription);
-    await this.page.pause();
     await this.actionFormPath.setInputFiles(this.actionPath);
     await this.page.getByRole('button', { name: 'Create' }).click();
     await this.waitForToast('Action Created Successfully');
