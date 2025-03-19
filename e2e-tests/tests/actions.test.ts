456cc3ce399f14fd723a083755084d5f42957d78
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-03-19 11:18:26 -0700

    Test skip

--- e2e-tests/tests/actions.test.ts
+++ e2e-tests/tests/actions.test.ts
@@ -11,9 +11,9 @@ let workspaceName: string = '';
 test.beforeAll(async ({ browser }) => {
   context = await browser.newContext();
   page = await context.newPage();
-
   action = new Action(page);
   sequence = new Sequence(page);
+
   await sequence.goto();
 });
 
@@ -26,25 +26,31 @@ test.describe.serial('Action', () => {
   test('Create new workspace', async () => {
     workspaceName = await sequence.createWorkspace();
   });
+
   test('Select workspace', async () => {
     await page.getByRole('gridcell', { name: workspaceName }).first().click();
   });
+
   test('Navigate to workspace actions', async () => {
     await page.getByRole('button', { name: 'Action' }).first().click();
     await page.waitForURL('/sequencing/actions?workspaceId=**');
     await page.waitForTimeout(250);
     await action.updatePage(page);
   });
+
   test('Create an action', async () => {
     await action.createAction();
   });
+
   test('Inspect an action', async () => {
     await action.inspectAction();
   });
-  test('Configure an action', async () => {
+
+  test.skip('Configure an action', async () => {
     await action.configureAction();
   });
-  test('Run an action', async () => {
+
+  test.skip('Run an action', async () => {
     await action.runAction();
   });
 });
