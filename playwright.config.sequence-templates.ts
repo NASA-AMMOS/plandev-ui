e720e8f26b7e891770e0f665e2abb6b34e0629c9
Bryan <bryan.duran@jpl.nasa.gov>
2025-03-26 13:54:21 -0700

    Sequence template e2e test running (#1656)
    add a separate e2e test suite for sequence templates

--- playwright.config.sequence-templates.ts
+++ playwright.config.sequence-templates.ts
@@ -5,7 +5,9 @@ import url from 'url';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const STORAGE_STATE = path.join(__dirname, 'e2e-test-results/.auth/user.json');
+const resultsPath = path.join('e2e-test-results/sequence-templates');
+
+export const STORAGE_STATE = path.join(__dirname, `e2e-test-results/.auth/user.json`);
 
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
@@ -16,15 +18,15 @@ const config: PlaywrightTestConfig = {
     },
     {
       dependencies: ['setup'],
-      name: 'e2e tests',
+      name: 'e2e sequence template tests',
       testDir: './e2e-tests',
-      testIgnore: /.*\/sequence-templates\.test\.ts/,
+      testMatch: /.*\/sequence-templates\.test\.ts/,
       use: {
         storageState: STORAGE_STATE,
       },
     },
     {
-      dependencies: ['e2e tests'],
+      dependencies: ['e2e sequence template tests'],
       name: 'teardown',
       testMatch: /global\.teardown\.ts/,
     },
@@ -35,9 +37,9 @@ const config: PlaywrightTestConfig = {
   },
   reporter: [
     [process.env.CI ? 'github' : 'list'],
-    ['html', { open: 'never', outputFile: 'index.html', outputFolder: 'e2e-test-results' }],
-    ['json', { outputFile: 'e2e-test-results/json-results.json' }],
-    ['junit', { outputFile: 'e2e-test-results/junit-results.xml' }],
+    ['html', { open: 'never', outputFile: 'index.html', outputFolder: resultsPath }],
+    ['json', { outputFile: `${resultsPath}/json-results.json` }],
+    ['junit', { outputFile: `${resultsPath}/junit-results.xml` }],
   ],
   retries: 2,
   testDir: './e2e-tests',
@@ -48,7 +50,7 @@ const config: PlaywrightTestConfig = {
     video: process.env.CI ? 'retain-on-failure' : 'off',
   },
   webServer: {
-    command: 'npm run preview',
+    command: 'PUBLIC_COMMAND_EXPANSION_MODE=templating npm run preview',
     port: 3000,
     reuseExistingServer: !process.env.CI,
   },
