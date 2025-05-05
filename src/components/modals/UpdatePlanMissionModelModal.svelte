032fdbd0a5a55f2a736d7869a612b74cc51a0535
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-05-05 13:25:53 -0700

    Block model migration when plan has incoming merge requests. Visual updates model incompatibility UI

--- src/components/modals/UpdatePlanMissionModelModal.svelte
+++ src/components/modals/UpdatePlanMissionModelModal.svelte
@@ -6,19 +6,25 @@
   import WarningIcon from '@nasa-jpl/stellar/icons/warning.svg?component';
   import type { ColDef, IRowNode, ValueGetterParams } from 'ag-grid-community';
   import { createEventDispatcher } from 'svelte';
+  import DirectiveIcon from '../../assets/timeline-directive.svg?component';
   import { models } from '../../stores/model';
+  import { gqlSubscribable } from '../../stores/subscribable';
   import type { ActivityDirective } from '../../types/activity';
   import type { User } from '../../types/app';
   import type { DataGridRowSelection, RowId } from '../../types/data-grid';
   import type { Model, ModelSlim } from '../../types/model';
-  import type { ModelCompatabilityForPlan, PlanSlim } from '../../types/plan';
+  import type { ModelCompatabilityForPlan, PlanMergeRequestSchema, PlanSlim } from '../../types/plan';
   import effects from '../../utilities/effects';
   import { getTarget } from '../../utilities/generic';
+  import gql from '../../utilities/gql';
+  import { getActivePlanMergeRequests } from '../../utilities/plan';
   import { pluralize } from '../../utilities/text';
   import { getShortISOForDate } from '../../utilities/time';
+  import { tooltip } from '../../utilities/tooltip';
   import Collapse from '../Collapse.svelte';
   import Input from '../form/Input.svelte';
   import Loading from '../Loading.svelte';
+  import AlertError from '../ui/AlertError.svelte';
   import CssGrid from '../ui/CssGrid.svelte';
   import CssGridGutter from '../ui/CssGridGutter.svelte';
   import DataGrid from '../ui/DataGrid/DataGrid.svelte';
@@ -71,6 +77,13 @@
   let selectedItemId: RowId | null = null;
   let selectedMissionModel: ModelSlim | null = null;
 
+  $: planMergeRequestsIncoming = gqlSubscribable<PlanMergeRequestSchema[] | null>(
+    gql.SUB_PLAN_MERGE_REQUEST_IDS_INCOMING,
+    { planId: plan.id },
+    null,
+    null,
+    incoming => getActivePlanMergeRequests(incoming),
+  );
   $: previewMissionModelMigration(selectedMissionModel);
   $: otherModels = $models.filter(m => m.id !== plan.model_id);
 
@@ -131,6 +144,13 @@
 <Modal {height} {width}>
   <ModalHeader on:close>Change Mission Model</ModalHeader>
   <div class="body">
+    {#if $planMergeRequestsIncoming !== null && $planMergeRequestsIncoming.length}
+      <AlertError
+        error="Cannot change model while plan has incoming merge requests"
+        fullError="All incoming merge requests for this plan must be resolved before model migration can be performed"
+        class="mb-3"
+      />
+    {/if}
     <Input>
       <div class="search-icon" slot="left">
         <SearchIcon />
@@ -148,6 +168,7 @@
         <DataGrid
           bind:currentSelectedRowId={selectedItemId}
           {columnDefs}
+          loading={!$planMergeRequestsIncoming}
           rowData={otherModels}
           rowSelection="single"
           on:rowSelected={onClickMissionModel}
@@ -158,32 +179,54 @@
       <CssGridGutter track={1} type="column" />
       <div class="model-migration-preview mission-model-column">
         {#if selectedMissionModel === null}
-          <div class="st-typography-label">Select mission model for expected conflicts...</div>
+          <div class="st-typography-label">Select mission model for expected incompatibilities...</div>
         {:else if loadingCompatibility}
           <Loading />
         {:else}
           <div>
-            <div class="st-typography-displayBody mb-2">Expected conflicts</div>
+            <div class="st-typography-displayBody mb-2">Expected Incompatibilities</div>
             {#if !migrationCompatibility}
-              <div class="st-typography-label mb-3 message">Unable to compute expected conflicts</div>
+              <div class="st-typography-label mb-3 message">Unable to compute expected incompatibilities</div>
             {:else if migrationCompatibility?.problematic_directives.length < 1}
-              <div class="st-typography-label mb-3 message"><CheckIcon /> No expected conflicts</div>
+              <div class="st-typography-label mb-3 message"><CheckIcon /> No expected incompatibilities</div>
             {:else}
               <div class="st-typography-body">
                 <div class="st-typography-body mb-3 message">
                   <WarningIcon class="red-icon" />
-                  {migrationCompatibility.problematic_directives.length} problematic activity directive{pluralize(
+                  {migrationCompatibility.problematic_directives.length} incompatible activity directive{pluralize(
                     migrationCompatibility.problematic_directives.length,
                   )}
                 </div>
                 {#if Object.keys(alteredDirectivesTypes).length > 0}
-                  <Collapse
-                    title="{Object.keys(alteredDirectivesTypes).length} Modified Activity Type{pluralize(
-                      Object.keys(alteredDirectivesTypes).length,
-                    )} ({alteredDirectivesCount} directive{pluralize(alteredDirectivesCount)})"
-                  >
+                  <Collapse>
+                    <div slot="title" class="collapse-title">
+                      {Object.keys(alteredDirectivesTypes).length} Modified Activity Type{pluralize(
+                        Object.keys(alteredDirectivesTypes).length,
+                      )}
+                      <div
+                        use:tooltip={{
+                          content: `${alteredDirectivesCount} affected activity directive${pluralize(alteredDirectivesCount)} in plan`,
+                        }}
+                        class="directives-badge"
+                      >
+                        <DirectiveIcon />
+                        {alteredDirectivesCount}
+                      </div>
+                    </div>
                     {#each Object.keys(alteredDirectivesTypes).sort() as type}
-                      <Collapse title="{type} ({alteredDirectivesTypes[type].length})" defaultExpanded={false}>
+                      <Collapse defaultExpanded={false} className="parameter-schema-collapse">
+                        <div slot="title" class="collapse-title">
+                          {type}
+                          <div
+                            use:tooltip={{
+                              content: `${alteredDirectivesTypes[type].length} affected activity directive${pluralize(alteredDirectivesTypes[type].length)} in plan`,
+                            }}
+                            class="directives-badge"
+                          >
+                            <DirectiveIcon />
+                            {alteredDirectivesTypes[type].length}
+                          </div>
+                        </div>
                         <CssGrid columns="50% 50%" gap="8px">
                           <div class="parameter-schema">
                             <div class="st-typography-label mb-1">
@@ -216,14 +259,33 @@
                   </Collapse>
                 {/if}
                 {#if Object.keys(removedDirectivesTypes).length > 0}
-                  <Collapse
-                    title="{Object.keys(removedDirectivesTypes).length} Removed Activity Type{pluralize(
-                      Object.keys(removedDirectivesTypes).length,
-                    )} ({removedDirectiveCount} directive{pluralize(removedDirectiveCount)})"
-                  >
+                  <Collapse>
+                    <div slot="title" class="collapse-title">
+                      {Object.keys(removedDirectivesTypes).length} Removed Activity Type{pluralize(
+                        Object.keys(removedDirectivesTypes).length,
+                      )}
+                      <div
+                        use:tooltip={{
+                          content: `${removedDirectiveCount} affected activity directive${pluralize(removedDirectiveCount)} in plan`,
+                        }}
+                        class="directives-badge"
+                      >
+                        <DirectiveIcon />
+                        {removedDirectiveCount}
+                      </div>
+                    </div>
                     {#each Object.keys(removedDirectivesTypes).sort() as type}
                       <div class="removed-type st-button tertiary">
-                        {type} ({removedDirectivesTypes[type].length})
+                        {type}
+                        <div
+                          use:tooltip={{
+                            content: `${removedDirectivesTypes[type].length} affected activity directive${pluralize(removedDirectivesTypes[type].length)} in plan`,
+                          }}
+                          class="directives-badge"
+                        >
+                          <DirectiveIcon />
+                          {removedDirectivesTypes[type].length}
+                        </div>
                       </div>
                     {/each}
                   </Collapse>
@@ -238,7 +300,13 @@
   <ModalFooter>
     <div class="st-typography-label">Snapshot will be automatically created</div>
     <button class="st-button secondary" on:click={close}>Cancel</button>
-    <button class="st-button" on:click={confirm} disabled={!selectedMissionModel}>Change Mission Model</button>
+    <button
+      class="st-button"
+      on:click={confirm}
+      disabled={!selectedMissionModel || !$planMergeRequestsIncoming || $planMergeRequestsIncoming.length > 0}
+    >
+      Change Mission Model
+    </button>
   </ModalFooter>
 </Modal>
 
@@ -308,7 +376,7 @@
   .removed-type {
     color: var(--st-typography-medium-color);
     cursor: default;
-    display: block;
+    gap: 8px;
     justify-content: flex-start;
     line-height: 24px;
     overflow: hidden;
@@ -319,5 +387,22 @@
 
   .removed-type.st-button:hover {
     background: inherit;
+  }
+
+  :global(.parameter-schema-collapse button.collapse-header) {
+    height: 24px !important;
+  }
+
+  .collapse-title {
+    display: flex;
+    gap: 8px;
+  }
+
+  .directives-badge {
+    align-items: center;
+    color: var(--st-gray-50);
+    display: flex;
+    flex-direction: row;
+    gap: 2px;
   }
 </style>
