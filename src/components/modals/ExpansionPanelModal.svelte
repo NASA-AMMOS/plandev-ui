47a8f9f32584872291bcaab6c403581108bbda23
JosephVolosin <josephvolosin@gmail.com>
2025-05-12 09:27:17 -0400

    Utilize Stellar components for new forms

--- src/components/modals/ExpansionPanelModal.svelte
+++ src/components/modals/ExpansionPanelModal.svelte
@@ -1,8 +1,13 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
+  import { Label, Select } from '@nasa-jpl/stellar-svelte';
   import { createEventDispatcher } from 'svelte';
+  import { field } from '../../stores/form';
   import { parcels, workspaces } from '../../stores/sequencing';
+  import type { Parcel, Workspace } from '../../types/sequencing';
+  import { min } from '../../utilities/validators';
+  import Field from '../form/Field.svelte';
   import Modal from './Modal.svelte';
   import ModalContent from './ModalContent.svelte';
   import ModalFooter from './ModalFooter.svelte';
@@ -16,16 +21,21 @@
     save: { parcelId: number; workspaceId: number };
   }>();
 
-  let workspaceId: number = -1;
-  let parcelId: number = -1;
+  let workspaceIdField = field<number>(-1, [min(1, 'Field is required')]);
+  let selectedWorkspace: Workspace | undefined;
+
+  let parcelIdField = field<number>(-1, [min(1, 'Field is required')]);
+  let selectedParcel: Parcel | undefined;
 
   let saveButtonDisabled: boolean = true;
 
-  $: saveButtonDisabled = workspaceId === -1 || parcelId === -1;
+  $: saveButtonDisabled = $workspaceIdField.value === -1 || $parcelIdField.value === -1;
+  $: selectedWorkspace = $workspaces.find(({ id }) => $workspaceIdField.value === id);
+  $: selectedParcel = $parcels.find(({ id }) => $parcelIdField.value === id);
 
   function save() {
     if (!saveButtonDisabled) {
-      dispatch('save', { parcelId, workspaceId });
+      dispatch('save', { parcelId: $parcelIdField.value, workspaceId: $workspaceIdField.value });
     }
   }
 
@@ -35,6 +45,20 @@
       event.preventDefault();
       save();
     }
+  }
+
+  function getDisplayNameForWorkspace(workspace?: Workspace) {
+    if (!workspace) {
+      return '';
+    }
+    return `${workspace.name} (${workspace.id})`;
+  }
+
+  function getDisplayNameForParcel(parcel?: Parcel) {
+    if (!parcel) {
+      return '';
+    }
+    return `${parcel.name} (${parcel.id})`;
   }
 </script>
 
@@ -46,34 +70,48 @@
   <ModalContent>
     <div class="st-typography-body">Select a workspace and parcel to use for sequencing.</div>
 
-    <fieldset>
-      <label for="workspaceId">Workspace ID</label>
-      <select data-type="number" name="workspaceId" bind:value={workspaceId} class="st-select w-100">
-        {#if !$workspaces.length}
-          <option value={-1}>No values</option>
-        {:else}
-          <option value={-1} />
-          {#each $workspaces as workspace}
-            <option value={workspace.id}>
-              {workspace.name} ({workspace.id})
-            </option>
-          {/each}
-        {/if}
-      </select>
-
-      <label for="parcelId">Parcel ID</label>
-      <select data-type="number" name="parcelId" bind:value={parcelId} class="st-select w-100">
-        {#if !$parcels.length}
-          <option value={-1}>No values</option>
-        {:else}
-          <option value={-1} />
-          {#each $parcels as parcel}
-            <option value={parcel.id}>
-              {parcel.name} ({parcel.id})
-            </option>
-          {/each}
-        {/if}
-      </select>
+    <fieldset class="send-to-workspace-form">
+      <Field field={workspaceIdField}>
+        <Label size="sm" for="workspace-id" class="pb-0.5">Workspace Id</Label>
+        <Select.Root
+          selected={{ label: getDisplayNameForWorkspace(selectedWorkspace), value: selectedWorkspace?.id ?? '' }}
+        >
+          <Select.Trigger class="min-w-[124px]" value={selectedWorkspace?.id} size="xs" aria-labelledby={null}>
+            <Select.Value aria-label="Select a sequencing workspace" placeholder="Select a sequencing workspace" />
+          </Select.Trigger>
+          <Select.Content class="z-[10000]">
+            {#each $workspaces as workspace}
+              <Select.Item
+                size="xs"
+                value={workspace.id}
+                label={getDisplayNameForWorkspace(workspace)}
+                class="flex gap-1"
+              >
+                {workspace.name}
+                <div class="whitespace-nowrap text-muted-foreground">(Id: {workspace.id})</div>
+              </Select.Item>
+            {/each}
+          </Select.Content>
+          <Select.Input type="number" name="workspace-id" aria-label="Select Workspace hidden input" />
+        </Select.Root>
+      </Field>
+      <Field field={parcelIdField}>
+        <Label size="sm" for="parcel-id" class="pb-0.5">Parcel Id</Label>
+        <Select.Root selected={{ label: getDisplayNameForParcel(selectedParcel), value: selectedParcel?.id ?? '' }}>
+          <Select.Trigger class="min-w-[124px]" value={selectedParcel?.id} size="xs" aria-labelledby={null}>
+            <Select.Value aria-label="Select a parcel" placeholder="Select a parcel" />
+          </Select.Trigger>
+          <Select.Content class="z-[10000]">
+            {#each $parcels as parcel}
+              <Select.Item size="xs" value={parcel.id} label={getDisplayNameForParcel(parcel)} class="flex gap-1">
+                {parcel.name}
+                <div class="whitespace-nowrap text-muted-foreground">(Id: {parcel.id})</div>
+              </Select.Item>
+            {/each}
+          </Select.Content>
+          <Select.Input type="number" name="parcel-id" aria-label="Select Parcel hidden input" />
+        </Select.Root>
+      </Field>
     </fieldset>
   </ModalContent>
 
@@ -82,3 +120,9 @@
     <button class="st-button" disabled={saveButtonDisabled} on:click={save}> Save </button>
   </ModalFooter>
 </Modal>
+
+<style>
+  .send-to-workspace-form {
+    gap: 4px;
+  }
+</style>
--- src/components/sequence-templates/NewSequenceTemplateModal.svelte
+++ src/components/sequence-templates/NewSequenceTemplateModal.svelte
@@ -1,21 +1,29 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
+  import { Input as InputStellar, Label, Select } from '@nasa-jpl/stellar-svelte';
   import { createEventDispatcher } from 'svelte';
+  import Field from '../../components/form/Field.svelte';
+  import { field } from '../../stores/form';
   import { models } from '../../stores/model';
   import { newTemplateActivityTypes, newTemplateModelId } from '../../stores/sequence-template';
   import { parcels } from '../../stores/sequencing';
+  import type { ActivityType } from '../../types/activity';
+  import type { ModelSlim } from '../../types/model';
+  import type { Parcel } from '../../types/sequencing';
+  import { min, required } from '../../utilities/validators';
   import Modal from '../modals/Modal.svelte';
   import ModalContent from '../modals/ModalContent.svelte';
   import ModalFooter from '../modals/ModalFooter.svelte';
   import ModalHeader from '../modals/ModalHeader.svelte';
 
   const heightNoImport = 380;
-  const heightWithImport = 450;
+  const heightWithImport = 410;
+  const importText = 'Import Template';
+  const hideImportText = 'Hide Import';
 
   export let height: number = heightNoImport;
   export let width: number = 380;
-  export let initialTemplateName: string = '';
 
   const dispatch = createEventDispatcher<{
     close: void;
@@ -36,49 +44,75 @@
     };
   }>();
 
-  let templateName: string = initialTemplateName;
-  let language: string = '';
-  let selectedParcelId: number | null = null;
-  let selectedActivityType: string = '';
+  let templateNameField = field<string>('', [required]);
+  let templateLanguageField = field<string>('', [required]);
+  let parcelIdField = field<number>(-1, [min(1, 'Field is required')]);
+  let modelIdField = field<number>(-1, [min(1, 'Field is required')]);
+  let activityTypeField = field<string>('', [required]);
+
+  let selectedParcel: Parcel | undefined;
+  let selectedModel: ModelSlim | undefined;
+  let selectedActivityType: ActivityType | undefined;
+  let orderedModels: ModelSlim[] = [];
+
   let showImport: boolean = false;
   let sequenceTemplateUploadFiles: FileList | undefined;
   let sequenceTemplateUploadFileInput: HTMLInputElement;
 
+  $: $newTemplateModelId = $modelIdField.value;
+  $: selectedParcel = $parcels.find(({ id }) => $parcelIdField.value === id);
+  $: selectedModel = $models.find(({ id }) => $modelIdField.value === id);
+  $: selectedActivityType = $newTemplateActivityTypes.find(
+    activityType => $activityTypeField.value === activityType.name,
+  );
+
+  // sort in descending ID order
+  $: orderedModels = [...$models].sort(({ id: idA }, { id: idB }) => {
+    if (idA < idB) {
+      return 1;
+    }
+    if (idA > idB) {
+      return -1;
+    }
+    return 0;
+  });
+
   $: saveButtonDisabled =
-    templateName === '' ||
-    language === '' ||
-    selectedParcelId === null ||
-    $newTemplateModelId === -1 ||
-    selectedActivityType === '' ||
-    selectedActivityType === null ||
+    $templateNameField.value === '' ||
+    $templateLanguageField.value === '' ||
+    $parcelIdField.value === -1 ||
+    $modelIdField.value === -1 ||
+    $activityTypeField.value === '' ||
     (showImport && sequenceTemplateUploadFiles === undefined);
 
-  $: height = heightWithImport ? showImport : heightNoImport;
+  $: height = showImport ? heightWithImport : heightNoImport;
 
   function save() {
     if (
       !saveButtonDisabled &&
-      $newTemplateModelId !== -1 &&
-      selectedParcelId !== null &&
-      selectedActivityType !== null
+      $activityTypeField.value !== '' &&
+      $templateLanguageField.value !== '' &&
+      $modelIdField.value !== -1 &&
+      $templateNameField.value !== '' &&
+      $parcelIdField.value !== -1
     ) {
       if (!showImport) {
         dispatch('save', {
-          activityType: selectedActivityType,
-          language,
+          activityType: $activityTypeField.value,
+          language: $templateLanguageField.value,
           modelId: $newTemplateModelId,
-          name: templateName,
-          parcelId: selectedParcelId,
+          name: $templateNameField.value,
+          parcelId: $parcelIdField.value,
         });
       } else if (sequenceTemplateUploadFiles !== undefined) {
         const sequenceTemplateUploadFile = sequenceTemplateUploadFiles[0];
         if (sequenceTemplateUploadFile !== undefined) {
           dispatch('import', {
-            activityType: selectedActivityType,
-            language,
+            activityType: $activityTypeField.value,
+            language: $templateLanguageField.value,
             modelId: $newTemplateModelId,
-            name: templateName,
-            parcelId: selectedParcelId,
+            name: $templateNameField.value,
+            parcelId: $parcelIdField.value,
             sequenceTemplateFile: sequenceTemplateUploadFile,
           });
         }
@@ -93,6 +127,20 @@
       save();
     }
   }
+
+  function getDisplayNameForParcel(parcel?: Parcel) {
+    if (!parcel) {
+      return '';
+    }
+    return `${parcel.name} (${parcel.id})`;
+  }
+
+  function getDisplayNameForModel(model?: ModelSlim) {
+    if (!model) {
+      return '';
+    }
+    return `${model.name} (Version: ${model.version})`;
+  }
 </script>
 
 <svelte:window on:keydown={onKeydown} />
@@ -101,90 +149,99 @@
   <ModalHeader on:close>Create Sequence Template</ModalHeader>
 
   <ModalContent>
-    <fieldset class="sequence-template-form">
-      <label for="name">Template Name</label>
-      <input bind:value={templateName} autocomplete="off" class="st-input w-100" id="name" required type="text" />
+    <Field field={templateNameField}>
+      <Label size="sm" for="template-name" class="pb-0.5">Template Name</Label>
+      <InputStellar id="template-name" autocomplete="off" sizeVariant="xs" name="name" aria-label="name" />
+    </Field>
 
-      <label for="language">Template Language</label>
-      <input bind:value={language} autocomplete="off" class="st-input w-100" id="language" required type="text" />
+    <Field field={templateLanguageField}>
+      <Label size="sm" for="template-language" class="pb-0.5">Template Language</Label>
+      <InputStellar id="template-language" autocomplete="off" sizeVariant="xs" name="language" aria-label="language" />
+    </Field>
 
-      <label for="parcelId">Parcel ID</label>
-      <select name="parcelId" bind:value={selectedParcelId} class="st-select w-100">
-        {#if !$parcels.length}
-          <option value={null}>No values</option>
-        {:else}
-          <option value={null} />
+    <Field field={parcelIdField}>
+      <Label size="sm" for="parcel-id" class="pb-0.5">Parcel Id</Label>
+      <Select.Root selected={{ label: getDisplayNameForParcel(selectedParcel), value: selectedParcel?.id ?? '' }}>
+        <Select.Trigger class="min-w-[124px]" value={selectedParcel?.id} size="xs" aria-labelledby={null}>
+          <Select.Value aria-label="Select a parcel" placeholder="Select a parcel" />
+        </Select.Trigger>
+        <Select.Content class="z-[10000]">
           {#each $parcels as parcel}
-            <option value={parcel.id}>
-              {parcel.name} ({parcel.id})
-            </option>
+            <Select.Item size="xs" value={parcel.id} label={getDisplayNameForParcel(parcel)} class="flex gap-1">
+              {parcel.name}
+              <div class="whitespace-nowrap text-muted-foreground">(Id: {parcel.id})</div>
+            </Select.Item>
           {/each}
-        {/if}
-      </select>
+        </Select.Content>
+        <Select.Input type="number" name="parcel-id" aria-label="Select Parcel hidden input" />
+      </Select.Root>
+    </Field>
 
-      <label for="modelId">Model ID</label>
-      <select data-type="number" name="modelId" bind:value={$newTemplateModelId} class="st-select w-100">
-        {#if !$models.length}
-          <option value={-1}>No values</option>
-        {:else}
-          <option value={-1} />
-          {#each $models as model}
-            <option value={model.id}>
-              {model.name} ({model.id})
-            </option>
+    <Field field={modelIdField}>
+      <Label size="sm" for="model" class="pb-0.5">Model</Label>
+      <Select.Root selected={{ label: getDisplayNameForModel(selectedModel), value: selectedModel?.id ?? '' }}>
+        <Select.Trigger value={selectedModel?.id} size="xs" aria-label="Select Model" aria-labelledby={null} id="model">
+          <Select.Value placeholder="Select a model" />
+        </Select.Trigger>
+        <Select.Content
+          class="z-[10000] min-w-[240px] overflow-auto p-0"
+          sameWidth={false}
+          align="start"
+          datatype="number"
+          fitViewport
+        >
+          {#each orderedModels as model (model.id)}
+            <Select.Item size="xs" value={model.id} label={getDisplayNameForModel(model)} class="flex gap-1">
+              {model.name}
+              <div class="whitespace-nowrap text-muted-foreground">(Version: {model.version})</div>
+            </Select.Item>
           {/each}
-        {/if}
-      </select>
+        </Select.Content>
+        <Select.Input type="number" name="model" aria-label="Select Model hidden input" />
+      </Select.Root>
+    </Field>
 
-      <label for="activityType">Activity Type</label>
-      <select
-        name="activityType"
-        bind:value={selectedActivityType}
-        class="st-select w-100"
+    <Field field={activityTypeField}>
+      <Label size="sm" for="activity-type" class="pb-0.5">Activity Type</Label>
+      <Select.Root
+        selected={{ label: selectedActivityType?.name ?? '', value: selectedActivityType?.name ?? '' }}
         disabled={$newTemplateModelId === -1}
       >
-        {#if !$newTemplateActivityTypes.length}
-          <option value={null}>No values</option>
-        {:else}
-          <option value={null} />
+        <Select.Trigger class="min-w-[124px]" value={selectedActivityType?.name} size="xs" aria-labelledby={null}>
+          <Select.Value aria-label="Select an activity type" placeholder="Select an activity type" />
+        </Select.Trigger>
+        <Select.Content class="z-[10000] max-h-48 overflow-y-scroll">
           {#each $newTemplateActivityTypes as activityType}
-            <option value={activityType.name}>
+            <Select.Item size="xs" value={activityType.name} label={activityType.name} class="flex gap-1">
               {activityType.name}
-            </option>
+            </Select.Item>
           {/each}
-        {/if}
-      </select>
+        </Select.Content>
+        <Select.Input type="string" name="activity-type" aria-label="Select Activity Type hidden input" />
+      </Select.Root>
+    </Field>
 
-      {#if showImport}
-        <label for="file">Imported File</label>
-        <div class="import-input-container">
-          <input
-            class="w-100"
-            name="file"
-            type="file"
-            bind:files={sequenceTemplateUploadFiles}
-            bind:this={sequenceTemplateUploadFileInput}
-          />
-        </div>
-      {/if}
-    </fieldset>
+    {#if showImport}
+      <fieldset>
+        <Label class="pb-0.5" size="sm" for="sequence-template-file">Sequence Template File</Label>
+        <!-- TODO consider porting the input files fix to stellar https://github.com/huntabyte/shadcn-svelte/pull/1700/files -->
+        <input
+          class="leading-1 flex h-6 w-full cursor-pointer content-center rounded-md border border-input px-2 pl-0 text-xs leading-5 ring-offset-background file:cursor-pointer file:border-0 file:bg-transparent file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring"
+          name="Sequence Template File"
+          id="sequence-template-file"
+          type="file"
+          bind:files={sequenceTemplateUploadFiles}
+          bind:this={sequenceTemplateUploadFileInput}
+        />
+      </fieldset>
+    {/if}
   </ModalContent>
 
   <ModalFooter>
     <button class="st-button secondary" on:click={() => dispatch('close')}> Cancel </button>
-    <button class="st-button secondary" on:click={() => (showImport = !showImport)}> Import Template </button>
+    <button class="st-button secondary" on:click={() => (showImport = !showImport)}>
+      {showImport ? hideImportText : importText}
+    </button>
     <button class="st-button" disabled={saveButtonDisabled} on:click={save}> Create Template </button>
   </ModalFooter>
 </Modal>
-
-<style>
-  .import-input-container {
-    column-gap: 0.5rem;
-    display: grid;
-    grid-template-columns: auto min-content;
-  }
-
-  .sequence-template-form {
-    gap: 4px;
-  }
-</style>
