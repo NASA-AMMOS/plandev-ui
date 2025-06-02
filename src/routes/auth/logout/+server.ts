43e9814a31e5ce5e1e86dd09f676a0be58f2c317
psubram3 <pranav.subramanian@nasa.gov>
2025-06-02 09:58:03 -0700

    update to allow logout to function

--- src/routes/auth/logout/+server.ts
+++ src/routes/auth/logout/+server.ts
@@ -1,20 +1,25 @@
 import { base } from '$app/paths';
+import { env } from '$env/dynamic/public';
 import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
 import { reqGatewayForwardCookies } from '../../../utilities/requests';
-import { env } from '$env/dynamic/public';
 
 export const POST: RequestHandler = async event => {
-  const invalidated =
+  const response =
     env.PUBLIC_AUTH_SSO_ENABLED === 'true'
-      ? await reqGatewayForwardCookies<boolean>('/auth/logoutSSO', event.request.headers.get('cookie') ?? '', base)
+      ? await reqGatewayForwardCookies<boolean | string>('/auth/logoutSSO', event.request.headers.get('cookie') ?? '', base)
       : true;
 
+    // NOTE: we MUST redirect to https://keycloak..../auth/realms/.../protocol/openid-connect/logout,
+    //    so the url is passed in success;
+    //    since we fetch to gateway (like normal) instead of redirecting to it or getting redirected back there 
+    //    like our standard login flow, we have to clear cookies here (as clearing on a fetch to gateway...does 
+    //    nothing.)
   return json(
-    { message: 'Logout successful', success: invalidated },
+    { message: 'Logout successful', success: response }, // response can be a string, but I wanted to change as little as possible in this fil, so the logic gets pushed to login.ts
     {
-      headers: {
-        'set-cookie': `activeRole=deleted; path=${base}/; expires=Thu, 01 Jan 1970 00:00:00 GMT,user=deleted; path=${base}/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
+      headers: { // we could clear cookies much more easily IF we had a /callback route on gateway. as a matter of fact, the purpose of that route is specifically for cookie storage (and removal), in the login/refresh/logout cases!
+        'set-cookie': `activeRole=deleted; path=${base}/; expires=Thu, 01 Jan 1970 00:00:00 GMT,user=deleted; path=${base}/; expires=Thu, 01 Jan 1970 00:00:00 GMT,access_token=deleted; path=${base}/; expires=Thu, 01 Jan 1970 00:00:00 GMT,refresh_token=deleted; path=${base}/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
       },
     },
   );
--- src/utilities/login.ts
+++ src/utilities/login.ts
@@ -11,12 +11,20 @@ export function shouldRedirectToLogin(user: User | null) {
 
 export async function logout(reason?: string) {
   if (browser) {
-    await fetch(`${base}/auth/logout`, { method: 'POST' });
-    if (env.PUBLIC_AUTH_SSO_ENABLED === 'true') {
-      // hooks will handle SSO redirect
-      await goto(base, { invalidateAll: true });
-    } else {
-      await goto(`${base}/login${reason ? '?reason=' + reason : ''}`, { invalidateAll: true });
+    const response = await fetch(`${base}/auth/logout`, { headers: {
+			'Content-Type': 'application/json'
+		}, method: 'POST' }).then(response => response.json());
+
+    if (typeof response.success === 'string') { // a Keycloak-auth specific case (or any OAuth involving the need to redirect to a logout page)
+      window.location.href = response.success; 
+    }
+    else {
+      if (env.PUBLIC_AUTH_SSO_ENABLED === 'true') { // CAM or other auth that doesn't require redirection
+        // hooks will handle SSO redirect
+        await goto(base, { invalidateAll: true });
+      } else {
+        await goto(`${base}/login${reason ? '?reason=' + reason : ''}`, { invalidateAll: true });
+      }
     }
   }
 }
