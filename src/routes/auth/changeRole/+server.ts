1d0f9e0c33b6e56797fe00020c692d02991d433d
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-08-12 12:13:05 -0700

    Refactor

--- src/routes/auth/changeRole/+server.ts
+++ src/routes/auth/changeRole/+server.ts
@@ -1,9 +1,8 @@
 import { base } from '$app/paths';
 import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
-import type { User } from '../../../types/app';
+import { computeRolesFromJWT } from '../../../hooks.server';
 import type { ChangeUserRoleRequestBody } from '../../../types/auth';
-import effects from '../../../utilities/effects';
 
 export const POST: RequestHandler = async event => {
   const body: ChangeUserRoleRequestBody = await event.request.json();
@@ -11,11 +10,7 @@ export const POST: RequestHandler = async event => {
   if (!event.locals.user) {
     return json({ message: 'User not found', success: false, user: null });
   }
-  const updatedUser: User = { ...event.locals.user, activeRole: role };
-  const permissibleQueries = await effects.getUserQueries(updatedUser);
-  const rolePermissions = await effects.getRolePermissions(updatedUser);
-  updatedUser.permissibleQueries = permissibleQueries;
-  updatedUser.rolePermissions = rolePermissions;
+  const updatedUser = await computeRolesFromJWT(event.locals.user, role);
   return json(
     { message: '', success: true, user: updatedUser },
     { headers: { 'set-cookie': `activeRole=${role}; Path=${base}/` } },
