872379884b2c954590fd8271da73c4dbf48056a9
David Legg <david.legg@jpl.nasa.gov>
2025-08-15 09:37:28 -0700

    Clean up SequenceTemplates
    Applies similar edits to SequenceTemplates & SequenceTemplateEditor as were applied to Sequences and SequenceEditor.
    These factor (some) language-specifics out to a separate package, loaded through the parcel.
    
    Notably, we decided to demand a "template language adaptation", rather than attempt to automatically produce a template adaptation from the regular language adaptation.
    This gives end users the ability to apply different functionality to templates and regular sequences, since templating may interfere with some sequence language adaptation features.
    
    This commit does not remove the "language" field from sequence templates, since that will require back end changes.
    It does remove virtually all the functionality tied to that field, though.

---
+++ src/language-package/languages/seq-n-handlebars/adaptation.ts
@@ -0,0 +1,51 @@
+import { indentService } from "@codemirror/language";
+import { EditorView } from "codemirror";
+import { debounce } from "lodash-es";
+import type { LanguageAdaptation, NewAdaptationInterface } from "../../interfaces/new-adaptation-interface";
+import { globals } from "../seq-n/global-types";
+import { seqNBlockHighlighter, seqNHighlightBlock } from "../seq-n/seq-n-highlighter";
+import { SeqNCommandInfoMapper } from "../seq-n/seq-n-tree-utils";
+import { seqNFormat, sequenceAutoIndent } from "../seq-n/sequence-autoindent";
+import { sequenceCompletion } from "../seq-n/sequence-completion";
+import { seqnLinter } from "../seq-n/sequence-linter";
+import { sequenceTooltip } from "../seq-n/sequence-tooltip";
+import { setupLanguageSupport } from "./seq-n-handlebars";
+
+const debouncedSeqNHighlightBlock = debounce(seqNHighlightBlock, 250);
+
+const seqnAdaptation: LanguageAdaptation = {
+    name: "SeqN (Template)",
+    fileExtension: ".template.seqN.txt",
+    editorExtension: context => [
+        setupLanguageSupport(sequenceCompletion(
+            context.channelDictionary,
+            context.commandDictionary,
+            context.parameterDictionaries,
+            Object.values(context.librarySequenceMap),
+        )),
+        seqnLinter(
+            globals,
+            context.channelDictionary,
+            context.commandDictionary,
+            context.parameterDictionaries,
+            Object.values(context.librarySequenceMap),
+        ),
+        sequenceTooltip(
+            context.channelDictionary,
+            context.commandDictionary,
+            context.parameterDictionaries,
+        ),
+        indentService.of(sequenceAutoIndent()),
+        [
+            EditorView.updateListener.of(debouncedSeqNHighlightBlock),
+            seqNBlockHighlighter,
+        ],
+    ],
+    commandInfoMapper: new SeqNCommandInfoMapper(),
+    format: seqNFormat,
+}
+
+export const defaultAdaptation: NewAdaptationInterface = {
+    input: seqnAdaptation,
+    outputs: [],
+}
