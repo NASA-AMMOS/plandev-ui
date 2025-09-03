57f34ca8e7eeeaf6bf3ad2928c529c92886af1cb
Timofej Ratsko <Timofej.Ratsko@jpl.nasa.gov>
2025-09-03 16:15:12 -0700

    Fix formatting

--- src/stores/simulation.ts
+++ src/stores/simulation.ts
@@ -107,7 +107,7 @@ export const allResourceTypes: Readable<ResourceType[]> = derived(
   ([$resourceTypes, $externalResources]) => {
     return $resourceTypes
       .map(({ name, schema, description }) => ({ description, name, schema }))
-      .concat($externalResources.map(({ name, schema }) => ({ description: undefined, name, schema  })));
+      .concat($externalResources.map(({ name, schema }) => ({ description: undefined, name, schema })));
   },
 );
 
