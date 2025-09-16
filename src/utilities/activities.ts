103a3f93005f9bd8b478680ea03a914ccbff058b
dandelany <dan.delany@gmail.com>
2025-09-16 12:52:15 -0700

    fix linter

--- src/utilities/activities.ts
+++ src/utilities/activities.ts
@@ -3,7 +3,7 @@ import type {
   ActivityDirective,
   ActivityDirectiveDB,
   ActivityDirectiveRevision,
-  ActivityDirectivesMap
+  ActivityDirectivesMap,
 } from '../types/activity';
 import type { ActivityMetadata, ActivityMetadataKey, ActivityMetadataValue } from '../types/activity-metadata';
 import type { Plan } from '../types/plan';
