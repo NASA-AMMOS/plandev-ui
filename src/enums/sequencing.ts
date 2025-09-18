ac457859b74433e6e34477ead998dee23de94d71
Carter Mak <Carter.Mak@jpl.nasa.gov>
2025-09-18 14:13:37 -0700

    Unused enum

--- src/enums/sequencing.ts
+++ src/enums/sequencing.ts
@@ -1,8 +1,3 @@
-export enum SequenceTypes { // TODO get from aerie-sequence-languages
-  LIBRARY = 'library',
-  USER = 'user',
-}
-
 export enum SequencingMode {
   TYPESCRIPT = 'typescript',
   TEMPLATING = 'templating',
