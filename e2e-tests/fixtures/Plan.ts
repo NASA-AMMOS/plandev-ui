01ffd99e6307271bba08ea1cae3aceea6f99ebff
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-09-22 12:47:30 -0700

    Clean

--- e2e-tests/fixtures/Plan.ts
+++ e2e-tests/fixtures/Plan.ts
@@ -550,8 +550,6 @@ export class Plan {
     this.externalSourceManageButton = page.getByLabel('Select derivation groups to');
     this.gridMenuButton = page.getByLabel('Plan Menu', { exact: true });
     this.gridMenu = page.getByRole('menu', { exact: true, name: 'Plan Menu' });
-    // this.gridMenuButton = page.locator('.grid-menu');
-    // this.gridMenu = this.gridMenuButton.getByRole('menu');
     this.gridMenuItem = (name: string) => this.gridMenu.getByRole('menuitem', { exact: true, name });
     this.navButtonActivityChecking = page.locator(`.nav-button:has-text("Activities")`);
     this.navButtonActivityCheckingMenu = this.navButtonActivityChecking.getByRole('menu');
