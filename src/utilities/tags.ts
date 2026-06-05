ab1439e2df3eec50ba1646aa60d03f32c80c42e9
dandelany <dan.delany@gmail.com>
2026-06-04 23:45:35 -0700

    improve diffTags function from code scan

--- src/utilities/tags.ts
+++ src/utilities/tags.ts
@@ -1,9 +1,13 @@
 import type { Tag } from '../types/tags';
 
 /**
- * Returns true if the IDs of tagsA match the order and IDs of tagsB.
+ * Returns true if tagsA and tagsB do not contain the same tag IDs.
  */
 export function diffTags(tagsA: Tag[], tagsB: Tag[]): boolean {
+  if (tagsA.length !== tagsB.length) {
+    return true;
+  }
+
   return (
     tagsA
       .map(tag => tag.id)
