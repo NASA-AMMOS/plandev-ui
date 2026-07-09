60c2244c557e971b21727c04e7b9740010c4ee8b
psubram3 <pranav.subramanian@nasa.gov>
2026-07-09 13:49:28 -0700

    update constraint tests pt 2

--- e2e-tests/tests/constraints.test.ts
+++ e2e-tests/tests/constraints.test.ts
@@ -1,7 +1,15 @@
-import test from '@playwright/test';
+import test, { expect } from '@playwright/test';
+import { adjectives, animals, colors, uniqueNamesGenerator } from 'unique-names-generator';
+import { Status } from '../../src/enums/status.js';
+import { PanelNames } from '../fixtures/Plan.js';
 import { setupTest, teardownTest, type FullSetupResult } from '../utilities/api.js';
 
 let setup: FullSetupResult;
+let originalConstraintName: string;
+const newConstraintName: string =
+  'FAILING_TEST_CONSTRAINT_' + uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] });
+const newConstraintDefinition: string =
+  "export default function peelFailing(): Constraint { return Real.Resource('/peel').lessThan(-1000); }";
 
 test.beforeAll(async ({ browser }) => {
   setup = await setupTest(browser);
@@ -21,8 +29,33 @@ test.describe.serial('Constraints', () => {
     await setup.plan.createConstraint(baseURL);
   });
 
+  test('Evaluate a failing and passing constraint', async ({ baseURL }) => {
+    originalConstraintName = setup.plan.constraints.constraintName;
+
+    // create a new constraint
+    setup.plan.constraints.constraintName = newConstraintName;
+    setup.plan.constraints.constraintDefinition = newConstraintDefinition;
+    await setup.plan.createConstraint(baseURL);
+
+    // simulate
+    await setup.plan.showPanel(PanelNames.SIMULATION, true);
+    await setup.plan.runSimulation(Status.Complete);
+
+    // evaluate constraints
+    await setup.page.getByRole('navigation').getByText('Constraints').click();
+    await setup.page.getByText('Check Constraints', { exact: true }).click();
+
+    // check results, should say "2 of 2 constraints, 1 of 1 violations"
+    await setup.plan.showPanel(PanelNames.CONSTRAINTS, true);
+    await expect(setup.page.getByText('2 of 2 constraints, 1 of 1').first()).toBeVisible({ timeout: 10000 });
+  });
+
   test('Delete constraint', async () => {
-    await setup.plan.removeConstraint();
-    await setup.constraints.deleteConstraint();
+    await setup.plan.removeConstraint(originalConstraintName);
+    await setup.plan.removeConstraint(newConstraintName);
+
+    await setup.page.pause();
+    await setup.constraints.deleteConstraint(originalConstraintName);
+    await setup.constraints.deleteConstraint(newConstraintName);
   });
 });
