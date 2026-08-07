56048e17a741be985b9db10cc47daca30a6da18a
Dan Delany <daniel.t.delany@jpl.nasa.gov>
2026-08-06 17:23:20 -0700

    remove deprecated sequence expansion components and replace pages with redirects

--- src/routes/expansion/rules/edit/[id]/+page.ts
+++ src/routes/expansion/rules/edit/[id]/+page.ts
@@ -1,26 +1,7 @@
-import { base } from '$app/paths';
+import { resolve } from '$app/paths';
 import { redirect } from '@sveltejs/kit';
-import effects from '../../../../../utilities/effects';
-import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ parent, params }) => {
-  const { user } = await parent();
-
-  const { id: ruleIdParam } = params;
-
-  if (ruleIdParam !== null && ruleIdParam !== undefined) {
-    const ruleIdAsNumber = parseFloat(ruleIdParam);
-
-    if (!Number.isNaN(ruleIdAsNumber)) {
-      const initialRule = await effects.getExpansionRule(ruleIdAsNumber, user);
-
-      if (initialRule !== null) {
-        return {
-          initialRule,
-        };
-      }
-    }
-  }
-
-  redirect(302, `${base}/expansion/rules`);
+export const load = async () => {
+  // TypeScript command expansion has been removed, replaced by Sequence Templates
+  redirect(308, resolve(`/sequence-templates`));
 };
